adb40d9e63f2328c058750dd0196b03f1616b733
Dmitry Ratnikov <ratnikov@google.com>
2021-11-25 16:55:47 -0500

    Switch most popen to async.

--- awesome/battery.lua
+++ awesome/battery.lua
@@ -1,41 +1,42 @@
+local awful = require("awful")
 local wibox = require("wibox")
 
 battery_widget = wibox.widget.textbox()
 battery_widget:set_align("right")
 
-function battery_markup()
+function battery_markup(cb)
   -- Use grep to disambiguate battery vs. USB hub by 'rate unformation unavalable'
-  local fd = io.popen("acpi | grep -v 'rate information unavailable'")
-  local acpi = fd:read("*all")
-  fd:close()
+  awful.spawn.easy_async_with_shell("acpi | grep -v 'rate information unavailable'", function(acpi)
+    local charge = tonumber(string.match(acpi, "(%d+)%%"))
 
-  local charge = tonumber(string.match(acpi, "(%d+)%%"))
+    local bg = ''
+    if tonumber(charge) <= 20 then
+      bg = 'red'
+    end
 
-  local bg = ''
-  if tonumber(charge) <= 20 then
-    bg = 'red'
-  end
+    -- If charging or full, always show green.
+    if string.match(acpi, "Discharging") == nil then
+      bg = 'green'
+    end
 
-  -- If charging or full, always show green.
-  if string.match(acpi, "Discharging") == nil then
-    bg = 'green'
-  end
-  
-  if not (bg == '') then
-    bg = string.format("background='%s'", bg)
-  end
+    if not (bg == '') then
+      bg = string.format("background='%s'", bg)
+    end
 
-  return string.format("<span %s>BAT=%s%%</span>", bg, charge)
+    cb(string.format("<span %s>BAT=%s%%</span>", bg, charge))
+  end)
 end
 
 function update_battery(w)
-	local markup = battery_markup()
-	
-	if markup == nil then
-		markup = string.format("<span background='red'>BAT N/A %s</span>", markup)
-	end
+  local cb = function(markup)
+    if markup == nil then
+      markup = string.format("<span background='red'>BAT N/A %s</span>", markup)
+    end
 
-	w:set_markup(markup)
+    w:set_markup(markup)
+  end
+
+  battery_markup(cb)
 end
 
 update_battery(battery_widget)
--- awesome/gcert.lua
+++ awesome/gcert.lua
@@ -1,3 +1,4 @@
+local awful = require("awful")
 local wibox = require("wibox")
 local util = require("util")
 
@@ -5,13 +6,13 @@ local w = wibox.widget.textbox()
 w:set_align("right")
 
 util.timed_widget(function()
-  local status = util.pread("nice -n 10 gcertstatus 2>&1")
-
-  if string.match(status, "file does not exist") or string.match(status, "expired") then
-    w:set_markup("<span color='red'>NO CERT</span>")
-  else
-    w:set_markup("")
-  end
+  awful.spawn.easy_async_with_shell("nice -n 10 gcertstatus 2>&1", function(status)
+    if string.match(status, "file does not exist") or string.match(status, "expired") then
+      w:set_markup("<span color='red'>NO CERT</span>")
+    else
+      w:set_markup("")
+    end
+  end)
 end, 10) -- Check every 10s to avoid too much spam.
 
 return w
